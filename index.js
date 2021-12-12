cd001ef9e7ad384caea9b048fa670b71608de4ed
Ayesha <ayesha@Fatehs-MacBook-Air.local>
2021-12-12 20:40:28 +0100

    akk

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+// Set EJS View Engine**
+app.set('view engine','ejs');
+// Set HTML engine**
+app.engine('html', require('ejs').renderFile);
+//set directory
+app.set('views', __dirname + '/views');
+//static folder
+app.use(express.static('public'));
+
+
+app.get('/', function(req, res) {
+    //open form.html from the views directory
+    res.render('index.html');
+});
+
+
+app.get('/search', urlencodedParser, function(req, res) {
+    //retrieve first and lastname
+    var searchText = req.query.searchText;
+    //open submitted.html after the user has submitted the form
+    res.cookie('searchText', searchText, { sameSite: 'none', secure: true}).render('mashup.html', {output: searchText});
+});
+
+app.post('/', urlencodedParser, function(req, res) {
+    //retrieve first and lastname
+    var searchText = req.body.searchText;
+    //open submitted.html after the user has submitted the form
+    res.cookie('searchText', searchText, { sameSite: 'none', secure: true}).render('mashup.html', {output: searchText});
+});
+
+app.listen(3000);
---
+++ public/disqus.js
@@ -0,0 +1,17 @@
+var input = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('searchText='))
+    .split('=')[1];
+
+
+var disqus_config = function () {
+this.page.url = 'https://ayesha-404.glitch.me/search?searchText=${input}';  
+this.page.identifier = '/search?searchText=${input}'; 
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://ayesha-1.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
