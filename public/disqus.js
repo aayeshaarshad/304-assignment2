90383bc2cc6918237374a1e651b1acac383eb9bf
Ayesha <ayesha@Fatehs-MacBook-Air.local>
2021-12-14 00:57:22 +0100

    updated

--- public/disqus.js
+++ public/disqus.js
@@ -4,14 +4,18 @@ var input = document.cookie
     .split('=')[1];
 
 
-var disqus_config = function () {
-this.page.url = 'https://ayesha1-404.glitch.me/search?searchText=' + input;  
-this.page.identifier = '/search?searchText=' + input; 
-};
+window.addEventListener('DOMContentLoaded', function () {
+    var disqus_config = function () {
+        this.page.url = 'https://ayesha1-404.glitch.me/search?searchText=' + input;
+        this.page.identifier = '/search?searchText=' + input;
+    };
 
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = 'https://ayesha-1.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
+    (function () { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://ayesha-1.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+});
+
+
