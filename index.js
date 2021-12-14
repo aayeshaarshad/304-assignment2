9f6a8ac7d61c3c905c8320e1a33587663b6a3908
Ayesha <ayesha@Fatehs-MacBook-Air.local>
2021-12-14 13:33:19 +0100

    updated

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var app = express();
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Set EJS View Engine**
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 // Set HTML engine**
 app.engine('html', require('ejs').renderFile);
 //set directory
@@ -17,29 +17,24 @@ app.set('views', __dirname + '/views');
 //static folder
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
 
-app.get('/', function(req, res) {
-    //open form.html from the views directory
+app.get('/', function (req, res) {
     res.render('index.html');
 });
 
 
-app.get('/search', urlencodedParser, function(req, res) {
+app.get('/search', urlencodedParser, function (req, res) {
     //retrieve first and lastname
     var searchText = req.query.searchText;
     //open submitted.html after the user has submitted the form
-    res.cookie('searchText', searchText, { sameSite: 'none', secure: true}).render('mashup.html', {output: searchText});
+    res.cookie('searchText', searchText, { sameSite: 'none', secure: true }).render('mashup.html', { output: searchText });
 });
 
-app.post('/', urlencodedParser, function(req, res) {
+app.post('/', urlencodedParser, function (req, res) {
     //retrieve first and lastname
     var searchText = req.body.searchText;
     //open submitted.html after the user has submitted the form
-    res.cookie('searchText', searchText, { sameSite: 'none', secure: true}).render('mashup.html', {output: searchText});
+    res.cookie('searchText', searchText, { sameSite: 'none', secure: true }).render('mashup.html', { output: searchText });
 });
 
 
--- public/search.js
+++ public/search.js
@@ -1,41 +1,51 @@
-const searchuniversity = async searchBox => {
-    const res = await fetch('/json/universityList.json');
+// const searchuniversity = async searchBox => {
 
-    const universities = await res.json();
+//     const res = await fetch('/json/universityList.json');
+//     const universities = await res.json();
     
-    //Get & Filter Through Entered Data
-    let fits = universities.filter(university => {
-      const regex = new RegExp(`^${searchBox}`, 'gi');
-      return university.name.match(regex);
-    });
+//     //Get & Filter Through Entered Data
+//     let fits = universities.filter(university => {
+//       const regex = new RegExp(`^${searchBox}`, 'gi');
+//       return university.name.match(regex);
+//     });
     
-    //Clears Data If Search Input Field Is Empty
-    if (searchBox.length === 0) {
-      fits = [];
-      universityList.innerHTML = '';
-    }
-    outputHtml(fits);
-  };
+//     //Clears Data If Search Input Field Is Empty
+//     if (searchBox.length === 0) {
+//       fits = [];
+//       universityList.innerHTML = '';
+//     }
+//     outputHtml(fits);
+//   };
 
 
-const outputHtml = fits => {
-    if (fits.length > 0) {
-      const html = fits
-        .map(
-          fit => `
-             <h4 class="card-title">${fit.name}</h4>`
-        )
-        .join('');
+// const outputHtml = fits => {
+//     if (fits.length > 0) {
+//       const html = fits
+//         .map(
+//           fit => `
+//              <h4 class="card-title">${fit.name}</h4>`
+//         )
+//         .join('');
   
-      document.getElementById('universityList').innerHTML = html;
+//       document.getElementById('universityList').innerHTML = html;
+//     }
+//   };
+
+
+  window.addEventListener('DOMContentLoaded', async () => {
+    const res = await fetch('/json/universityList.json');
+    const universities = await res.json();
+    console.log(universities);
+    var selectHTML = '<option value=""> Select a University..</option>';
+    for (const [key, value] of Object.entries(universities)) {
+        selectHTML+= '<option value="' + value.name + '">' + value.name + '</option>';
     }
-  };
+    console.log(selectHTML);
+    document.getElementById('search').innerHTML= selectHTML;
+  });
 
-  document
-  .getElementById('search')
-  .addEventListener('input', () => searchuniversity(search.value));
-
-  document.getElementById('universityList').addEventListener('click', (event) => {
-    document.getElementById('search').value = event.target.innerHTML;
-  })
+ // document .getElementById('search').addEventListener('input', () => {searchuniversity(search.value)});
+//   document.getElementById('universityList').addEventListener('click', (event) => {
+//   document.getElementById('search').value = event.target.innerHTML;
+//  })
   
